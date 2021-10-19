8a957d02d001cd181ba9aae46307308405547572
HumphreyThabeng <91993837+HumphreyThabeng@users.noreply.github.com>
2021-10-19 12:20:28 +0200

    Add files via upload

---
+++ task_1.3.py
@@ -0,0 +1,8 @@
+First = int(input("Enter your first number: "))
+Second = int(input("Enter your second number: "))
+z = First + Second
+
+if First and Second ==65 or z ==65:
+    print("True")
+else: print("False")
+
