2146251e09512081b0daafddb4fae0faa32314e6
HumphreyThabeng <91993837+HumphreyThabeng@users.noreply.github.com>
2021-10-19 12:16:34 +0200

    Add files via upload

---
+++ task_1.2.py
@@ -0,0 +1,9 @@
+First = int(input("Enter your first number: "))
+Second = int(input("Enter your second number: "))
+z = First + Second
+
+if First !=3 and Second !=3:
+    print ("False")
+elif '3' in str(z):
+    print("True")
+
